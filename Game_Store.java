9e912c8a87ca9171b1fee891f8e17223d4c0379d
Ustymscorpion <Ustym.horin@gmail.com>
2022-05-07 11:29:47 +0200

    edit file

--- Game_Store.java
+++ Game_Store.java
@@ -1,19 +1,27 @@
+import java.util.Scanner;
+
 public class Game_Store {
     public static void main(String[] args){
+
     Game game1 = new Game();
     // створення об'єкту класу Game. game1 - об'єкт
     game1.setTitle("Minecraft");
     game1.setYear(2009);
     game1.setGenre("Sandbox, survival");
 
-    game1.displayGameInfo();
 
      Game game2 = new Game();
      game2.setTitle("Cyberpunk 2077");
      game2.setYear(2020);
      game2.setGenre("Action role-playing");
 
-     game2.displayGameInfo();
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if ( num==1 ) {
+            game1.displayGameInfo();
+        }
+
 
     }
 }
